ddf7c84c50c0a24e57c97ff32cb94898f121ad04
IST <IST@192.168.1.17>
2017-08-02 17:51:35 +0530

    Demo project

---
+++ QuickProWeb/src/main/java/com/quickpro/App.java
@@ -0,0 +1,15 @@
+package com.quickpro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+        System.out.print( "Hello World! \t" );
+        System.out.print( "Hello World!" );
+    }
+}
---
+++ QuickProWeb/src/test/java/com/quickpro/AppTest.java
@@ -0,0 +1,20 @@
+package com.quickpro;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+    
+{
+    @Test
+    public void firsttest(  )
+    {
+        this.setname() ;
+    	System.out.println(" Hello world");
+    }
+
+    public void setname(){
+    	
+    	System.out.println(" Hello world:::::Atul");
+    }
+
+}
